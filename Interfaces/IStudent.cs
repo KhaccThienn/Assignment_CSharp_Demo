b8ad50345aa7e3f7665df45fe6cff57e387c81b1
Lê Khắc Thiện <muzankibut112@gmail.com>
2024-08-16 11:25:02 +0700

    Add project files.

---
+++ Interfaces/IStudent.cs
@@ -0,0 +1,18 @@
+﻿using Assignment_CSharp_Demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_CSharp_Demo.Interfaces
+{
+    public interface IStudent
+    {
+        void InputData();
+        void Display();
+
+        List<Student> Search(double n, double m);
+        List<Student> Search(string name);
+    }
+}
